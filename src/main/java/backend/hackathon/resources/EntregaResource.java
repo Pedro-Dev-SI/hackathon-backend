7af4b86834761278ffb01b4fa9b41d7a8bb40ba4
Pedro Selvate <pedro.selvate@youxgroup.com.br>
2022-10-23 06:41:06 -0300

    Mappings

--- src/main/java/backend/hackathon/resources/EntregaResource.java
+++ src/main/java/backend/hackathon/resources/EntregaResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,13 @@ import backend.hackathon.domains.Entrega;
 import backend.hackathon.service.EntregaService;
 
 @RestController
-@RequestMapping("entregas")
+@RequestMapping("/entregas")
 public class EntregaResource {
 	
 	@Autowired
 	EntregaService entregaService;
 	
+	@GetMapping("/retornar-entregas")
 	public ResponseEntity<List<Entrega>> findAllEntregas() {
 		return ResponseEntity.ok().body(entregaService.findAllEntrega());
 	}
